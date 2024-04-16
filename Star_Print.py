385906b45d518f4876df0698293de62273717f1d
ShamShamsw <jhaseman3@gmail.com>
2024-04-16 12:45:53 -0500

    update 2

---
+++ Star_Print.py
@@ -0,0 +1,12 @@
+def star_print():
+    for i in range(1, 6):
+        print("*" * i)
+
+star_print()
+
+#sudo code
+#1. define function star_print
+#2. The function should run a for loop from 1 to 5
+#3. The function should print the number of stars equal to the number of the loop
+#4. Call the star_print function
+#5. end
--- pythagorean_Theorem.py
+++ pythagorean_Theorem.py
@@ -6,7 +6,7 @@ b = int(input("Enter the value of b: "))
 print("The value c is: ", pythag(a, b))
 
 #sudo code
-#1. Define a function called "pythag" that takes two inputs, a and b
+#1. Define a function called pythag that takes two inputs, a and b
 #2. The function should return the square root of a squared plus b squared
 #3. Take an integer input a and b from the user.
 #4. Call the function "pythag" and print the result
